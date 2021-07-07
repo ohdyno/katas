4eddd268a8a5ab441eab2b033d088c954c24d7ad
Xing Zhou <xzhou110@ford.com>
2021-07-07 09:21:07 -0400

    feat: implement rps without the observer pattern

--- rps/rps.js
+++ rps/rps.js
@@ -1,7 +1,10 @@
-
-
-function returnTrue() {
-  return true;
+function rps(player1, player2) {
+  if (player1 === "rock" && player2 === "scissors"
+    || player1 === "scissors" && player2 === "paper") {
+    return "player 1 wins";
+  }
+  return "player 2 wins";
 }
-module.exports = returnTrue;
+
+module.exports = rps;
 
--- rps/rps.test.js
+++ rps/rps.test.js
@@ -1,4 +1,21 @@
 const rps = require("./rps");
-test("should return something", () => {
-  expect(rps()).toBe(true);
+describe("player 1 win scenarios", () => {
+  test("rock vs scissors", () => {
+    expect(rps("rock", "scissors")).toEqual("player 1 wins");
+  });
+  test("scissors vs paper", () => {
+    expect(rps("scissors", "paper")).toEqual("player 1 wins");
+  });
+  test("scissors vs paper", () => {
+    expect(rps("scissors", "paper")).toEqual("player 1 wins");
+  });
+});
+
+describe("player 2 win scenarios", () => {
+  test("scissors vs rock", () => {
+    expect(rps("scissors", "rock")).toEqual("player 2 wins");
+  });
+  test("rock vs paper", () => {
+    expect(rps("rock", "paper")).toEqual("player 2 wins");
+  });
 });
