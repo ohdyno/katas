49da9fddd4203572a3a92f9f1270f96630af8ac7
Xing Zhou <xzhou110@ford.com>
2021-07-08 09:54:06 -0400

    feat: remove request and response implementation of RPS

--- rps/rps.js
+++ rps/rps.js
@@ -1,13 +1,5 @@
 module.exports = {
-  requestResponseRps: function(player1, player2) {
-    if (player1 === "rock" && player2 === "scissors"
-      || player1 === "scissors" && player2 === "paper") {
-      return "player 1 wins";
-    }
-    return "player 2 wins";
-  },
-
-  observerRps: function(player1, player2, ui) {
+  rps: function(player1, player2, ui) {
     if (player1 === "rock" && player2 === "scissors"
       || player1 === "scissors" && player2 === "paper") {
       ui.playerOneWins();
--- rps/rps.test.js
+++ rps/rps.test.js
@@ -1,23 +1,4 @@
-const { requestResponseRps, observerRps } = require("./rps");
-
-describe("in request/response pattern", () => {
-  describe("player 1 win scenarios", () => {
-    test("rock vs scissors", () => {
-      expect(requestResponseRps("rock", "scissors")).toEqual("player 1 wins");
-    });
-    test("scissors vs paper", () => {
-      expect(requestResponseRps("scissors", "paper")).toEqual("player 1 wins");
-    });
-  });
-  describe("player 2 win scenarios", () => {
-    test("scissors vs rock", () => {
-      expect(requestResponseRps("scissors", "rock")).toEqual("player 2 wins");
-    });
-    test("rock vs paper", () => {
-      expect(requestResponseRps("rock", "paper")).toEqual("player 2 wins");
-    });
-  });
-});
+const { rps } = require("./rps");
 
 describe("in observer pattern", () => {
   let uiSpy;
@@ -31,24 +12,24 @@ describe("in observer pattern", () => {
 
   describe("player 1 win scenarios", () => {
     test("rock vs scissors", () => {
-      observerRps("rock", "scissors", uiSpy)
+      rps("rock", "scissors", uiSpy)
       expect(uiSpy.playerOneWins).toHaveBeenCalled();
       expect(uiSpy.playerTwoWins).not.toHaveBeenCalled();
     });
     test("scissors vs paper", () => {
-      observerRps("scissors", "paper", uiSpy)
+      rps("scissors", "paper", uiSpy)
       expect(uiSpy.playerOneWins).toHaveBeenCalled();
       expect(uiSpy.playerTwoWins).not.toHaveBeenCalled();
     });
   });
   describe("player 2 win scenarios", () => {
     test("scissors vs rock", () => {
-      observerRps("scissors", "rock", uiSpy)
+      rps("scissors", "rock", uiSpy)
       expect(uiSpy.playerTwoWins).toHaveBeenCalled();
       expect(uiSpy.playerOneWins).not.toHaveBeenCalled();
     });
     test("rock vs paper", () => {
-      observerRps("rock", "paper", uiSpy)
+      rps("rock", "paper", uiSpy)
       expect(uiSpy.playerTwoWins).toHaveBeenCalled();
       expect(uiSpy.playerOneWins).not.toHaveBeenCalled();
     });
