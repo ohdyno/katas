c1aa82f26399fc6994613798992d6928eefe814d
HassanHarajly <hharajl1@MGC12F64GGMD6N.fbpld77.ford.com>
2021-07-09 12:01:10 -0400

    mob next [ci-skip] [ci skip] [skip ci]

--- rps-react/src/App.test.js
+++ rps-react/src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("spec name", () => {
+  //given player 1 chooses rock and player 2 chooses scissors
+
+  //when the component renders
+
+  const rpsStub = (player1, player2, ui) => {
+    ui.playerOneWins();
+  };
+  render(<App rps={rpsStub} />);
+  //then player 1 wins is displayed
+  const winnerText = screen.getByText(/player 1 wins/i);
+  expect(winnerText).toBeInTheDocument();
 });
