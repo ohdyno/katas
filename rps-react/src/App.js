eb4c21a19dea52c1d3512d28be7f9d1ceda4b7fc
Xing Zhou <xzhou110@ford.com>
2021-07-13 10:02:23 -0400

    feat: user can play a rps round through react

--- rps-react/src/App.js
+++ rps-react/src/App.js
@@ -1,29 +1,40 @@
 import "./App.css";
+import { useState } from "react";
 
 function App(props) {
-  let winner = '';
-  let ui = {
+  const [winner, setWinner] = useState("no rounds played yet");
+  const ui = {
     playerOneWins() {
-      winner = "player 1 wins";
+      setWinner("player 1 wins");
     },
 
     playerTwoWins() {
-      winner = "player 2 wins";
+      setWinner("player 2 wins");
     }
   };
 
-  let playerOneChoice = "";
-  let playerTwoChoice = "";
+  const [playerOneChoice, setPlayerOneChoice] = useState("");
+  const [playerTwoChoice, setPlayerTwoChoice] = useState("");
 
   function playRps() {
     props.rps(playerOneChoice, playerTwoChoice, ui);
   }
 
+  function updatePlayerTwoChoice(event) {
+    setPlayerTwoChoice(event.target.value);
+  }
+
+  function updatePlayerOneChoice(event) {
+    setPlayerOneChoice(event.target.value);
+  }
+
   return (
     <div className="App">
-      <input value={playerOneChoice} data-testid={"playerOne"} />
-      <input value={playerTwoChoice} data-testid={"playerTwo"} />
-      <button data-testid={"play"} onClick={playRps} />
+      <label htmlFor={"playerOne"}>Player One Choice</label>
+      <input id={"playerOne"} value={playerOneChoice} onChange={updatePlayerOneChoice} data-testid={"playerOne"} />
+      <label htmlFor={"playerTwo"}>Player Two Choice</label>
+      <input id={"playerTwo"} value={playerTwoChoice} onChange={updatePlayerTwoChoice} data-testid={"playerTwo"} />
+      <button data-testid={"play"} onClick={playRps}>Play Round</button>
       <div>
         {winner}
       </div>
--- rps-react/src/App.test.js
+++ rps-react/src/App.test.js
@@ -23,20 +23,16 @@ it("given player two wins, when the app renders, then the app shows 'player 2 wi
   expect(winnerText).toBeInTheDocument();
 });
 
-it("given the user filled in 'rock' for player one and 'scissors' for player two, when the user clicks 'play', then the user sees 'player 1 wins'", () => {
-  // given
+it("given the user filled in 'rock' for player one and 'scissors' for player two," +
+  "when the user clicks 'play'," +
+  "then the user sees 'player 1 wins'", () => {
   render(<App rps={rps} />);
 
-  // fill text field with 'rock'
-  userEvent.type(screen.getByTestId("playerOne"), "rock");
-  // fill text field with 'scissor'
-  userEvent.type(screen.getByTestId("playerTwo"), "scissors");
+  userEvent.type(screen.getByLabelText(/player one choice/i), "rock");
+  userEvent.type(screen.getByLabelText(/player two choice/i), "scissors");
 
-  // when
-  // click the play button
-  userEvent.click(screen.getByTestId("play"));
+  userEvent.click(screen.getByText(/play round/i));
 
-  // then
   const winnerText = screen.getByText(/player 1 wins/i);
   expect(winnerText).toBeInTheDocument();
 });
